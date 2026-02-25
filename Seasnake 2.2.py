6b15d8f6f85ec65eed8a9f2f81c497616309ca14
Simon-Panz <bingobongo1561@gmail.com>
2026-02-25 15:46:35 +0100

    Nicht-visuelles in update verschoben, Fish kann nicht mehr in Snake spawnen, 1-2 unnütze Sachen entfernt

--- Seasnake 2.2.py
+++ Seasnake 2.2.py
@@ -9,13 +9,14 @@ WIDTH = 560
 HEIGHT = 700
 
 # Verbindung mit Lighthouse herstellen
-#conn = Pyghthouse(username, token)
-#Pyghthouse.start(conn)
+conn = Pyghthouse(username, token)
+Pyghthouse.start(conn)
 
 # Globale Variablen für das Spiel
 player = [{"x": 14, "y": 7}]
 gamestate = "intro"	# Spielstatus zu Beginn "intro"
 direction = 0		# zu Beginn keine Richtung
+player_len = 1      # Spielerlänge zu Beginn
 
 # Fish
 fish = 0
@@ -35,17 +36,12 @@ def draw():
     white = (255, 255, 255)
     gelb = (255, 255, 102)
 
-    
     # Zeichne aktuelles Bild
     image = Pyghthouse.empty_image()
 
-    # Titelbild
-    if gamestate == "intro":
-        direction = 0	# Richtung auf 0 setzen, sonst bewegt sich die Figur wieder, wenn man zurück ins Spiel geht
-        gamestate_intro(image, gelb, blue)
-
-
     # ingame Darstellung:
+    if gamestate == "intro":
+        gamestate_intro(image, gelb, blue)
     elif gamestate == "game":
         # Hintergrund darstellen:
         for y in range(14):
@@ -62,47 +58,13 @@ def draw():
                 image[player[n]["y"]][player[n]["x"]] = (255, 255, 0)
     
         sleep(0.25)		#Geschwindigkeit vom Player
-        
-        
-        
-        # Bewegung der Schlange
-        if direction != 0:
-            head = {"x": player[0]["x"],"y": player[0]["y"]}	# head ist der neue Kopf, mit dem geprüft wird, was als nächstes passiert
-            
-        
-            # Bewege Spielfigur nach links, wenn Richtung = "left":
-            if direction == "left":
-                    head["x"] -= 1
-            # Bewege Spielfigur nach rechts, wenn Richtung = "right":
-            elif direction == "right":
-                    head["x"] += 1
-            # Bewege Spielfigur nach unten, wenn Richtung = "down":
-            elif direction == "down":
-                    head["y"] += 1
-            # Bewegt Spielfigur nach unten, wenn Richtung = "down"
-            elif direction == "up":
-                    head["y"] -= 1
-            
-            # Wenn Schlange auf Wand trifft, gameover:
-            if head["x"] == -1 or head["x"] == 28 or head["y"] == -1 or head["y"] == 14:
-                gamestate = "gameover"
-            elif head in fishlist:	# Schlange verlängern, falls man auf Fisch trifft
-                player.insert(0, head)
-            else:					# Schlange bewegen, gleiche Länge
-                player.insert(0, head)
-                player.pop(-1)
-            if head in player[1:]:		# Schlange trifft sich selbst, gameover
-                gamestate = "gameover"
-            
-            
-    
+
     # Game Over Screen
     elif gamestate == "gameover":
-        direction = 0	# Richtung wieder am 0 setzen
         gamestate_gameover(image, black, red)
 
     # Schicke aktuelles Bild an Lighthouse-Server
-    #Pyghthouse.set_image(conn, image)
+    Pyghthouse.set_image(conn, image)
 
     # Stelle Bild zusätzlich im Pygame-Fenster dar
     screen.clear()
@@ -114,7 +76,7 @@ def draw():
 
 # Verarbeite gedrückte Taste (wird bei Tastendruck aufgerufen)
 def on_key_down(key):
-    global gamestate, direction, fish, fishlist, player
+    global gamestate, direction, fish, fishlist, player, player_len
     # Beendet Programm, falls Escape-Taste gedrückt wurde
     if key == keys.ESCAPE and gamestate == "intro":
         Pyghthouse.close(conn)
@@ -122,11 +84,8 @@ def on_key_down(key):
     # Mit Escape-Taste kommt man zurück zum Titelbild, falls man im Spiel ist
     elif key == keys.ESCAPE and gamestate == "game":
         gamestate = "intro"
-    # Mit esc auch aus Gameover Screen ins Spiel zurück
-    elif key == keys.ESCAPE and gamestate == "gameover":
-        gamestate = "game"
-    # Mit Space-Taste kommt man ins Spiel, wenn man gerade nicht im Spiel ist
-    elif key == keys.SPACE and gamestate != "game":
+    # Mit Space-Taste kommt man ins Spiel, wenn man gerade nicht im Spiel ist, mit esc aus dem Todesscreen ins Spiel
+    elif key == keys.SPACE and gamestate != "game" or key == keys.ESCAPE and gamestate == "gameover":
         gamestate = "game"
         player[0]["x"] = 14
         player[0]["y"] = 7
@@ -134,33 +93,35 @@ def on_key_down(key):
         fishlist = []
         fish = 0
         player[1:] = []
+        player_len = 1
      # Richtung nach links ändern
-     elif (key == keys.LEFT or key == keys.A) and direction != "right":
+    elif (key == keys.LEFT or key == keys.A) and direction != "right":
              direction = "left"
      # Richtung nach rechts ändern
-     elif (key == keys.RIGHT or key == keys.D) and direction != "left":
+    elif (key == keys.RIGHT or key == keys.D) and direction != "left":
              direction = "right"
      # Richtung nach unten ändern
-     elif (key == keys.DOWN or key == keys.S) and direction != "up":
+    elif (key == keys.DOWN or key == keys.S) and direction != "up":
              direction = "down"
      # Richtung nach oben ändern
-     elif (key == keys.UP or key == keys.W) and direction != "down":
+    elif (key == keys.UP or key == keys.W) and direction != "down":
              direction = "up"
 
 
 # Aktualisiere Spielzustand (wird ca. 60-mal pro Sekunde aufgerufen)
 def update(dt):
-    global gamestate, fish, fishlist, player
+    global gamestate, fish, fishlist, player, direction, player_len
 
     # Fish
     def fishcheck():
         global fishlist
         global fish
         if fish == 0:
-            fish += 1
-            for n in range(fish):
+            while fish == 0:
                 n = {"x": random.randint(0, 27), "y": random.randint(0, 13)}
-                fishlist.append(n)
+                if n not in player:
+                    fishlist.append(n)
+                    fish += 1
 
     fishcheck()
     # CONSUME
@@ -168,7 +129,41 @@ def update(dt):
         fish -= 1
         fishlist.pop(0)
     fishcheck()
-    pass
+
+    # Bewegung der Schlange
+    if direction != 0:
+        head = {"x": player[0]["x"],
+                "y": player[0]["y"]}  # head ist der neue Kopf, mit dem geprüft wird, was als nächstes passiert
+
+        # Bewege Spielfigur nach links, wenn Richtung = "left":
+        if direction == "left":
+            head["x"] -= 1
+        # Bewege Spielfigur nach rechts, wenn Richtung = "right":
+        elif direction == "right":
+            head["x"] += 1
+        # Bewege Spielfigur nach unten, wenn Richtung = "down":
+        elif direction == "down":
+            head["y"] += 1
+        # Bewegt Spielfigur nach unten, wenn Richtung = "down"
+        elif direction == "up":
+            head["y"] -= 1
+
+        # Wenn Schlange auf Wand trifft, gameover:
+        if head["x"] == -1 or head["x"] == 28 or head["y"] == -1 or head["y"] == 14:
+            gamestate = "gameover"
+        elif head in fishlist:  # Schlange verlängern, falls man auf Fisch trifft
+            player.insert(0, head)
+            player_len += 1
+            if player_len == 392:
+                gamestate = "win"
+        else:  # Schlange bewegen, gleiche Länge
+            player.insert(0, head)
+            player.pop(-1)
+        if head in player[1:]:  # Schlange trifft sich selbst, gameover
+            gamestate = "gameover"
+
+        if gamestate == "intro" or gamestate == "gameover":
+            direction = 0  # Richtung auf 0 setzen, sonst bewegt sich die Figur wieder, wenn man zurück ins Spiel geht
 
 
 # Starte Pygame Zero
